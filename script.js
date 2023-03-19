89ac32deb574e2423709a2e584054593b7b34310
GUSILLUS <zebela2002gmail.com>
2023-03-20 00:35:20 +0200

    Added: timer, flipper, win\lose message, refresh button

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card"),
+timeTag = document.querySelector(".time b"),
+flipsTag = document.querySelector(".flips b"),
+refreshBtn = document.querySelector(".details button"),
+cardsContainer = document.querySelector(".cards");
 
+let maxTime = 45;
+let timeLeft = maxTime;
+let flips = 0;
 let matched = 0;
-let cardOne, cardTwo;
+let cardOne, cardTwo, timer;
 let disableDeck = false;
+let isPlaying = false;
+
+function initTimer() {
+  if(timeLeft <= 0) {
+    lose();
+
+    return clearInterval(timer);
+  }
+  timeLeft--;
+  timeTag.textContent = timeLeft;
+}
 
 function flipCard({target: clickedCard}) {
-    if(cardOne !== clickedCard && !disableDeck) {
-        clickedCard.classList.add("flip");
-        if(!cardOne) {
-            return cardOne = clickedCard;
-        }
-        cardTwo = clickedCard;
-        disableDeck = true;
-        let cardOneImg = cardOne.querySelector(".back-view img").src,
-        cardTwoImg = cardTwo.querySelector(".back-view img").src;
-        matchCards(cardOneImg, cardTwoImg);
+  if (!isPlaying) {
+    isPlaying = true;
+    timer = setInterval(initTimer, 1000);
+  }
+
+  if (clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
+    flips++;
+    flipsTag.textContent = flips;
+    clickedCard.classList.add("flip");
+    if(!cardOne) {
+      return cardOne = clickedCard;
     }
+    cardTwo = clickedCard;
+    disableDeck = true;
+    let cardOneImg = cardOne.querySelector(".back-view img").src,
+      cardTwoImg = cardTwo.querySelector(".back-view img").src;
+      matchCards(cardOneImg, cardTwoImg);
+  }
 }
 
 function matchCards(img1, img2) {
     if(img1 === img2) {
         matched++;
-        if(matched == 8) {
-            setTimeout(() => {
-                return shuffleCard();
-            }, 1000);
+        if(matched === 8 && timeLeft > 0) {
+            win();
+            return clearInterval(timer);
         }
         cardOne.removeEventListener("click", flipCard);
         cardTwo.removeEventListener("click", flipCard);
@@ -45,21 +69,66 @@ function matchCards(img1, img2) {
 }
 
 function shuffleCard() {
-    matched = 0;
-    disableDeck = false;
-    cardOne = cardTwo = "";
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
-    cards.forEach((card, i) => {
-        card.classList.remove("flip");
-        let imgTag = card.querySelector(".back-view img");
-        imgTag.src = `images/image_${arr[i]}.png`;
-        card.addEventListener("click", flipCard);
-    });
+
+  if (document.querySelector('.message')) {
+    document.querySelector('.message').remove();
+    cardsContainer.classList.toggle('blur');
+  }
+
+  timeLeft = maxTime;
+  flips = matched = 0;
+  cardOne = cardTwo = "";
+
+  clearInterval(timer);
+  timeTag.textContent = timeLeft;
+  flipsTag.textContent = flips;
+
+  disableDeck = isPlaying = false;
+
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+  arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+  cards.forEach((card, i) => {
+    card.classList.remove("flip");
+    let imgTag = card.querySelector(".back-view img");
+    imgTag.src = `images/image_${arr[i]}.png`;
+    card.addEventListener("click", flipCard);
+  });
 }
 
 shuffleCard();
+
+refreshBtn.addEventListener("click", shuffleCard)
     
 cards.forEach(card => {
     card.addEventListener("click", flipCard);
 });
+
+function win() {
+  const message = document.createElement('div');
+
+  message.classList.add('message')
+  message.innerHTML = `
+    <img class="message_img" src="images/win.png" alt="message-img">
+    <p class="message_text">Victory has been claimed!<br> The battle is won!</p>
+    <img class="message_img" src="images/win_1.png" alt="message-img">
+  `;
+
+  cardsContainer.classList.toggle('blur');
+
+  document.body.append(message);
+}
+
+function lose() {
+  const message = document.createElement('div');
+
+  message.classList.add('message')
+  message.innerHTML = `
+    <img class="message_img" src="images/lose.png" alt="message-img">
+    <p class="message_text">Alas! Our defeat is certain.<br>The battle is lost.</p>
+    <img class="message_img" src="images/lose_1.png" alt="message-img">
+  `;
+
+  cardsContainer.classList.toggle('blur');
+
+  document.body.append(message);
+}
