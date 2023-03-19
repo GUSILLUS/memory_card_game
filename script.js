abc6205047d7ec64ecfb7e91efec37e8092b82f1
GUSILLUS <zebela2002gmail.com>
2023-03-19 20:56:12 +0200

    Script bug fix

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function shuffleCard() {
     cards.forEach((card, i) => {
         card.classList.remove("flip");
         let imgTag = card.querySelector(".back-view img");
-        imgTag.src = `/images/image_${arr[i]}.png`;
+        imgTag.src = `images/image_${arr[i]}.png`;
         card.addEventListener("click", flipCard);
     });
 }
